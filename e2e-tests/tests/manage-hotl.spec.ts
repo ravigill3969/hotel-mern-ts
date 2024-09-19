514e75a8dd0fc9a5a1c211ce09901546b57adde6
RavisherSinghGill <gill1122ravi@gmail.com>
2024-09-19 17:16:26 -0400

    e2e tests on update hotel info

--- e2e-tests/tests/manage-hotl.spec.ts
+++ e2e-tests/tests/manage-hotl.spec.ts
@@ -59,3 +59,23 @@ test("should display hotels list", async ({ page }) => {
   await expect(page.getByRole("link", { name: "View Details" })).toBeVisible();
   await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
 });
+
+test("should edit hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+  await page.getByRole("link", { name: "View Details" }).first().click();
+
+  await page.waitForSelector('[name="name"]', { state: "attached" });
+  await expect(page.locator('[name="name"]')).toHaveValue("Dublin Getaways");
+  await page.locator('[name="name"]').fill("Dublin Getaways Updated");
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel updated successfully")).toBeVisible();
+
+  await page.reload();
+
+  await expect(page.locator('[name="name"]')).toHaveValue(
+    "Dublin Getaways Updated"
+  );
+  await page.locator('[name="name"]').fill("Dublin Getaways");
+  await page.getByRole("button", { name: "Save" }).click();
+});
