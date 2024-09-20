b3466ebd9050a7d03eac6e5b0d6ff361369f691d
RavisherSinghGill <gill1122ravi@gmail.com>
2024-09-20 11:28:41 -0400

    added session storage

--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -26,13 +26,13 @@ type SearchContextProviderProps = {
 export const SearchContextProvider = ({
   children,
 }: SearchContextProviderProps) => {
-  const [destination, setDestination] = useState<string>("");
-  const [checkIn, setCheckIn] = useState<Date>(new Date());
-  const [checkOut, setCheckOut] = useState<Date>(new Date());
-  const [adultCount, setAdultCount] = useState<number>(1);
-  const [childCount, setChildCount] = useState<number>(0);
+  const [destination, setDestination] = useState<string>(()=>sessionStorage.getItem("destination") || "");
+  const [checkIn, setCheckIn] = useState<Date>(()=>new Date(sessionStorage.getItem("checkIn") || new Date().toISOString()));
+  const [checkOut, setCheckOut] = useState<Date>(()=>new Date(sessionStorage.getItem("checkOut") || new Date().toISOString()));
+  const [adultCount, setAdultCount] = useState<number>(()=>Number(sessionStorage.getItem("adultCount") || 1));
+  const [childCount, setChildCount] = useState<number>(()=>Number(sessionStorage.getItem("childCount") || 0));
 
-  const [hotelId, setHotelId] = useState<string>("");
+  const [hotelId, setHotelId] = useState<string>(()=>sessionStorage.getItem("hotelId") || "");
 
   const saveSearchValues = (
     destination: string,
@@ -40,7 +40,7 @@ export const SearchContextProvider = ({
     checkOut: Date,
     adultCount: number,
     childCount: number,
-    hotelId?: string
+    hotelId?: string,
   ) => {
     setDestination(destination);
     setCheckIn(checkIn);
@@ -49,6 +49,15 @@ export const SearchContextProvider = ({
     setChildCount(childCount);
     if (hotelId) {
       setHotelId(hotelId);
+    }
+
+    sessionStorage.setItem("destination", destination);
+    sessionStorage.setItem("checkIn", checkIn.toISOString());
+    sessionStorage.setItem("checkOut", checkOut.toISOString());
+    sessionStorage.setItem("adultCount", adultCount.toString());
+    sessionStorage.setItem("childCount", childCount.toString());
+    if (hotelId) {
+      sessionStorage.setItem("hotelId", hotelId);
     }
   };
 
